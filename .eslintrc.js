7db67cc321f7d89a854330f4422bc19c127d701b
Dyovana <dyo.menegatti@hotmail.com>
2025-06-02 18:44:06 -0300

    estrutura inicial do vue

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    // Pode adicionar regras aqui
+  }
+};
